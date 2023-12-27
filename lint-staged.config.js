8d2ef5dd432b93abc3d865280d37397416146f70
lemanh-tuong <letuong05092000@gmail.com>
2023-12-27 20:58:54 +0700

    fix: fix stylelint không hoạt động + lint-staged báo lỗi absolute path

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,16 +1,29 @@
+const path = require('path');
+
+const currentWorkingDirectory = process.cwd();
+
 module.exports = {
   // Typecheck
-  "{apps,libs,tools}/**/*.{ts,tsx}": (files) => {
-    return `nx affected --target=typecheck --files=${files.join(",")}`;
+  '{apps,libs,tools}/**/*.{ts,tsx}': (files) => {
+    const filesJoinded = files
+      .map((item) => path.relative(currentWorkingDirectory, item))
+      .join(',');
+    return `nx affected --target=typecheck --files=${filesJoinded}`;
   },
 
   // Lint
-  "{apps,libs,tools}/**/*.{js,ts,jsx,tsx,json}": (files) => {
-    return `nx affected --target=lint --files=${files.join(",")}`;
+  '{apps,libs,tools}/**/*.{js,ts,jsx,tsx,json}': (files) => {
+    const filesJoinded = files
+      .map((item) => path.relative(currentWorkingDirectory, item))
+      .join(',');
+    return `nx affected --target=lint --files=${filesJoinded}`;
   },
 
-  // Stylelint
-  "{apps,libs,tools}/**/*.{js,ts,jsx,tsx,json}": (files) => {
-    return `nx affected --target=stylelint --files=${files.join(",")}`;
+  // // Stylelint
+  '{apps,libs,tools}/**/*.{js,ts,jsx,tsx,json}': (files) => {
+    const filesJoinded = files
+      .map((item) => path.relative(currentWorkingDirectory, item))
+      .join(',');
+    return `nx affected --target=stylelint --files=${filesJoinded}`;
   },
 };
