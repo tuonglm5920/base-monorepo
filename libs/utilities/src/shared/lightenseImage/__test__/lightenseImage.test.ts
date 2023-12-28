39ca8928a1724e81a06d7ef431f4c0124b8648b8
lemanh-tuong <letuong05092000@gmail.com>
2023-12-28 09:47:02 +0700

    feature: thêm lib LightenseImage

---
+++ libs/utilities/src/shared/lightenseImage/__test__/lightenseImage.test.ts
@@ -0,0 +1,14 @@
+describe('LightenseImage', () => {
+  it('TODO: Should render correctly when click to open', () => {
+    expect(true).toEqual(true);
+  });
+  it('TODO: Should render correctly when click to close', () => {
+    expect(true).toEqual(true);
+  });
+  it('TODO: Should close correctly when scroll', () => {
+    expect(true).toEqual(true);
+  });
+  it('TODO: Should render correctly when resize', () => {
+    expect(true).toEqual(true);
+  });
+});
